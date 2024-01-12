0db8e3080cd2d738c5204b55bb5944724b2fa762
wildy13 <wildy.simanjuntak13@gmail.com>
2024-01-12 16:19:52 +0700

    features(api): add create/mkdir folder

--- api/module/controller.js
+++ api/module/controller.js
@@ -2,6 +2,13 @@ import slug from 'slug';
 import Modules from './model.js';
 import Project from '../project/model.js';
 
+import { mkdir, rmdir } from 'node:fs/promises';
+import { join } from 'node:path';
+import fileDirName from '../utils/file-dir-name.js';
+
+const { __dirname } = fileDirName(import.meta);
+const publicFolder = `${__dirname}/../../web/public`;
+
 export const getAll = async (req, res) => {
     try {
         const modules = await Modules.find({})
@@ -39,14 +46,18 @@ export const create = async (req, res) => {
             path: 'signTo',
             select: '-password  -role',
         });
-
+        await mkdir(join(publicFolder, modules.slug), { recursive: true });
         prjt = await Project.findById(project);
         prjt.module.push(modules._id);
         await prjt.save();
 
         res.status(200).send(modules);
     } catch (error) {
-        res.status(500).send(error)
+        if (error.code === 11000) {
+            res.status(400).send({ message: 'Duplicated data, please review your input!' });
+        } else {
+            res.status(500).send(error);
+        }
     }
 }
 
@@ -74,7 +85,8 @@ export const remove = async (req, res) => {
     try {
         await Promise.all(
             req.body.map(async (v) => {
-                await Modules.findOneAndDelete({ _id: v._id })
+                const item =  await Modules.findOneAndDelete({ _id: v._id });
+                await rmdir(join(publicFolder, item.slug), { recursive: true });
             })
         );
         res.status(200).send(req.body);
